c8e1363b8fc358302b17a3f3c3bb158991ea358c
lishu <lishugupta652@gmail.com>
2021-12-04 23:32:11 +0530

    styled-component header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,28 @@ import styled from "styled-components";
 
 import { StyledHeader } from "./Header.styled";
 
+const Navbar = styled.nav`
+  grid-area: navbar;
+  background-color: ${(props) => props.theme.header.nav.background};
+  color: ${(props) => props.theme.header.nav.color};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-transform: uppercase;
+  padding: 0 20px;
+`;
+
 const Header = () => {
   return (
     <div>
       <StyledHeader class="header">
-        <div class="navbar">
+        <Navbar class="navbar">
           <h2>lishu gupta</h2>
           <ul>
             <li>blog</li>
             <li>log in</li>
           </ul>
-        </div>
+        </Navbar>
         <div class="headerText"></div>
         <div class="headerSidebar"></div>
       </StyledHeader>
