69858e1f88f89b3e842a367bf53698e334138e34
lishu <lishugupta652@gmail.com>
2021-12-04 22:09:34 +0530

    theme

--- src/contextapi/ThemeContext.js
+++ src/contextapi/ThemeContext.js
@@ -1,9 +1,12 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const Theme = createContext();
 
 export const ThemeContext = (props) => {
-  return <Theme.Provider value="light">{props.children}</Theme.Provider>;
+  const [theme, setTheme] = useState("dark");
+  return (
+    <Theme.Provider value={[theme, setTheme]}>{props.children}</Theme.Provider>
+  );
 };
 
 export default ThemeContext;
