c0c9a4113a17848d2187a170fbf8a8afb34bd9f6
lishu <lishugupta652@gmail.com>
2021-12-05 04:09:03 +0530

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { StyledAbout } from "./About.styled";
 
+// Animation on Scroll
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+
 const About = () => {
+  const { inView, entry, ref } = useInView();
   return (
     <StyledAbout>
       <h1>SKILLS</h1>
-      <div class="skills">
+      <div class="skills" ref={ref}>
         <div class="card one"></div>
         <div class="card two"></div>
         <div class="card three"></div>
