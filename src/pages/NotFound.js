718d163b3d4a876215c09827bdc1587cfcff9a6c
lishu <lishugupta652@gmail.com>
2021-12-06 01:43:45 +0530

    not found route

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,10 +4,29 @@ import Skills from "../components/About/Skills";
 import Header from "../components/Header/Header";
 import Work from "../components/Work/Work";
 
+import NotFoundGif from "../images/notfound.gif";
+
+const StyledNotFound = styled.div`
+  min-height: 500px;
+
+  .heading h1 {
+    font-size: clamp(3rem, 4.5vw, 5rem);
+  }
+  img {
+    width: 100%;
+    border-radius: 20px;
+  }
+`;
+
 const NotFound = () => {
   return (
     <div>
       <Header />
+      <StyledNotFound>
+        <div className="heading">
+          <img src={NotFoundGif} alt="" />
+        </div>
+      </StyledNotFound>
     </div>
   );
 };
