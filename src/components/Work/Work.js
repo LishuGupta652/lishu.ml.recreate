d74f6be577a2687d111094cc12ff6467ed13b70b
lishu <lishugupta652@gmail.com>
2021-12-05 14:30:15 +0530

    project works

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -3,6 +3,9 @@ import { StyledWork } from "./Work.styled";
 
 import { motion } from "framer-motion";
 
+// images
+import dwarf from "../../images/project/dwarf.jpg";
+
 const Skills = () => {
   const textVariants = {
     visible: { opacity: 1, y: 0 },
@@ -11,8 +14,21 @@ const Skills = () => {
 
   return (
     <StyledWork>
-      <div className="container01">
-        <h1>dwarf</h1>
+      <h2>projects</h2>
+      <div class="flex-container">
+        <div class="flex-items item01">
+          <img src={dwarf} alt="" />
+        </div>
+        <div class="flex-items item02">
+          <div className="card">
+            <div className="head">
+              <h1>dwarf.co.in</h1>
+            </div>
+            <div className="body">
+              Complete Responsive Design made with ReactJS, Firebase
+            </div>
+          </div>
+        </div>
       </div>
     </StyledWork>
   );
--- src/components/Work/Work.styled.js
+++ src/components/Work/Work.styled.js
@@ -1,3 +1,65 @@
 import styled from "styled-components";
 
-export const StyledWork = styled.div``;
+export const StyledWork = styled.div`
+  margin-top: 100px;
+  color: ${(props) => props.theme.colors.color};
+
+  h2 {
+    text-align: center;
+    text-transform: uppercase;
+    margin: 30px;
+    font-size: clamp(1rem, 3.5vw, 2.5rem);
+  }
+
+  .flex-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: normal;
+    align-items: stretch;
+    align-content: stretch;
+  }
+
+  .flex-items:nth-child(1) {
+    display: block;
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+    align-self: auto;
+    order: 0;
+    width: 60%;
+  }
+  .item01 img {
+    width: 100%;
+    border-radius: 20px;
+  }
+
+  .flex-items:nth-child(2) {
+    display: block;
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+    align-self: auto;
+    order: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40%;
+  }
+  .item02 {
+  }
+
+  .card {
+    margin: 10px 30px;
+    padding: 20px;
+    background-color: ${(props) => props.theme.colors.greenTrans};
+    border-radius: 10px;
+  }
+  .card .head {
+    margin: 10px 0;
+    text-align: center;
+  }
+  .card .body {
+    text-align: justify;
+  }
+`;
