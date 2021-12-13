6de528cf35f5214c0b2d3d88e366726d95a46562
lishu <lishugupta652@gmail.com>
2021-12-13 14:52:23 +0530

    helmet updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,44 +15,6 @@ const Home = () => {
 
   return (
     <div>
-      <Helmet>
-        <meta
-          name="description"
-          content="A young and passionate developer. Working with Javascript (NodeJS, Express, ReactJS, React Native), Dart (Flutter), Python (ML)."
-        />
-        <link rel="apple-touch-icon" href="lishu.jpg" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-        <title>Lishu gupta | Portfolio</title>
-        <meta name="title" content="Lishu gupta | Portfolio" />
-        <meta
-          name="description"
-          content="A young and passionate developer. Working with Javascript (NodeJS, Express, ReactJS, React Native), Dart (Flutter), Python (ML)."
-        />
-
-        <meta
-          name="keywords"
-          content=" Lishu gupta, lishu, lishugupta652, gitman, pakkabaniya"
-        />
-        <meta name="author" content="Lishu Gupta" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.lishu.ml/" />
-        <meta property="og:title" content="Lishu gupta | Portfolio" />
-        <meta
-          property="og:description"
-          content="A young and passionate developer. Working with Javascript (NodeJS, Express, ReactJS, React Native), Dart (Flutter), Python (ML)."
-        />
-        <meta property="og:image" content="/lishu.jpg" />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.lishu.ml/" />
-        <meta property="twitter:title" content="Lishu gupta | Portfolio" />
-        <meta
-          property="twitter:description"
-          content="A young and passionate developer. Working with Javascript (NodeJS, Express, ReactJS, React Native), Dart (Flutter), Python (ML)."
-        />
-        <meta property="twitter:image" content="/lishu.jpg" />
-      </Helmet>
       <Header />
       <Skills />
       <Work showFeatured={true} />
